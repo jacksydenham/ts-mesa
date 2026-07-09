5e6929d6daaecd2e3f2033ad088df3941bf591c6
Jack <jack.sydenham@live.com.au>
2026-07-09 13:48:15 +1000

    feat(0217): add second, constant memory solution for Contains Duplicate with nested testing

--- src/217-contains-duplicate/solution.test.ts
+++ src/217-contains-duplicate/solution.test.ts
@@ -1,14 +1,31 @@
-import { containsDuplicate } from "./solution";
+import { containsDuplicate, containsDuplicateCM } from "./solution";
+
 describe('Contains Duplicate', () => {
-    test('Should return true if duplicate exists', () => {
-        expect(containsDuplicate([1,2,3,4,4,5])).toBe(true);
+    describe('CD standard set-based solution', () => {
+        test('Should return true if duplicate exists', () => {
+            expect(containsDuplicate([1,2,3,4,4,5])).toBe(true);
+        });
+
+        test('Should return false on no duplicate', () => {
+            expect(containsDuplicate([9,145,12,5])).toBe(false);
+        })
+
+        test('Shold return false when no values exist', () => {
+            expect(containsDuplicate([])).toBe(false);
+        })
+    })
+
+    describe('CD with sorted solution', () => {
+        test('Should return true if duplicate exists', () => {
+            expect(containsDuplicateCM([1,2,3,4,4,5])).toBe(true);
+        });
+
+        test('Should return false on no duplicate', () => {
+            expect(containsDuplicateCM([9,145,12,5])).toBe(false);
+        })
+
+        test('Shold return false when no values exist', () => {
+            expect(containsDuplicateCM([])).toBe(false);
+        })
     });
-
-    test('Should return false on no duplicate', () => {
-        expect(containsDuplicate([9,145,12,5])).toBe(false);
-    })
-
-    test('Shold return false when no values exist', () => {
-        expect(containsDuplicate([])).toBe(false);
-    })
 });
--- src/217-contains-duplicate/solution.ts
+++ src/217-contains-duplicate/solution.ts
@@ -9,3 +9,16 @@ export function containsDuplicate(nums: number[]): boolean {
     
     return false;
 }
+
+// if we want to save on memory at the cost of speed, we can write a constant memory
+//  solution which introduces a sort, bringing our time to o(n log n)
+export function containsDuplicateCM(nums: number[]): boolean {
+
+    nums.sort((a,b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i+1]) return true;
+    }
+
+    return false;
+}
