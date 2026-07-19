c65127ebe0db8a2595ec8f55a44b6a9517243b02
Jack <jack.sydenham@live.com.au>
2026-07-19 16:13:16 +1000

    feat(1773): follow-up cleaner solutions for count items matching a rule

--- src/problems/1773-count-items-matching-a-rule/solution.test.ts
+++ src/problems/1773-count-items-matching-a-rule/solution.test.ts
@@ -1,7 +1,15 @@
-import { countMatches } from './solution';
+import { countMatches, countMatchesCleanly, countMatchesEvenMoreCleanly } from './solution';
 
 describe('Count Items Matching A Rule', () => {
-    test('sample test case', () => {
+    test('should return correct output for initial implementation', () => {
         expect(countMatches([["phone","blue","pixel"],["laptop","silver","phone"],["phone","gold","iphone"]], "type", "phone")).toBe(2)
+    });
+
+    test('should return correct output for cleaner implementation', () => {
+        expect(countMatchesCleanly([["phone","blue","pixel"],["laptop","silver","phone"],["phone","gold","iphone"]], "type", "phone")).toBe(2)
+    });
+
+    test('should return correct output for final implementation', () => {
+        expect(countMatchesEvenMoreCleanly([["phone","blue","pixel"],["laptop","silver","phone"],["phone","gold","iphone"]], "type", "phone")).toBe(2)
     });
 });
--- src/problems/1773-count-items-matching-a-rule/solution.ts
+++ src/problems/1773-count-items-matching-a-rule/solution.ts
@@ -15,3 +15,34 @@ export function countMatches(items: string[][], ruleKey: string, ruleValue: stri
 
     return matches;
 };
+
+// Making more of an effort to avoid non-null asserts, we can solve this with a more dynamic filter.
+export function countMatchesCleanly(items: string[][], ruleKey: string, ruleValue: string): number {
+
+    // tighter type definitions. more readable? maybe... also JS maps obj keys as strings so we don't need "..."
+    const ruleMap: Record<"type" | "color" | "name", number> = {
+        type: 0,
+        color: 1,
+        name: 2
+    }
+
+    // cast ruleKey as key of the type of the map.
+    const index = ruleMap[ruleKey as keyof typeof ruleMap];
+    
+    // then we just return the length of an array of items which only include items with the matching ruleValue.
+    return items.filter(item => item[index] === ruleValue).length;
+}
+
+// This is great for leet, but really it's not very readable. and the main problem is that 'keyof typeof' assertion.
+// in the real world, we can just strictly type the ruleKey as the options we have, and the solution becomes far simpler.
+export function countMatchesEvenMoreCleanly(items: string[][], ruleKey: "type" | "color" | "name", ruleValue: string): number {
+
+    const ruleMap = {
+        type: 0,
+        color: 1,
+        name: 2
+    }
+
+    const index = ruleMap[ruleKey];
+    return items.filter(item => item[index] === ruleValue).length;
+}
